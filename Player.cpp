9f05e8ac468dc407242d7fb3f10145e8d72b3ebf
YoueG <Youe.Graillot@etu.udamail.fr>
2017-10-03 12:14:27 +0200

    Screen Loop

--- Player.cpp
+++ Player.cpp
@@ -17,22 +17,22 @@ void Player::update(float delta)
 	if (GetAsyncKeyState(VK_UP))
 	{
 		m_charInfos[1].Char.AsciiChar = 'X';
-		setVelocity(Vector2(0, -0.000001));
+		setVelocity(Vector2(0, -0.000002));
 	}
 	else if (GetAsyncKeyState(VK_DOWN))
 	{
 		m_charInfos[1].Char.AsciiChar = 'X';
-		setVelocity(Vector2(0, 0.000001));
+		setVelocity(Vector2(0, 0.000002));
 	}
 	else if (GetAsyncKeyState(VK_LEFT))
 	{
 		m_charInfos[1].Char.AsciiChar = 'X';
-		setVelocity(Vector2(-0.000001, 0));
+		setVelocity(Vector2(-0.000002, 0));
 	}
 	else if (GetAsyncKeyState(VK_RIGHT))
 	{
 		m_charInfos[1].Char.AsciiChar = 'X';
-		setVelocity(Vector2(0.000001, 0));
+		setVelocity(Vector2(0.000002, 0));
 	}
 
 	DynamicEntity::update(delta);
@@ -40,11 +40,19 @@ void Player::update(float delta)
 
 void Player::drawEntity(CHAR_INFO* buffer)
 {
-	if (m_posInfos.x >= 0 && m_posInfos.x < WIDTH)
-		if (m_posInfos.y >= 0 && m_posInfos.y < HEIGHT)
-		{
-			buffer[m_posInfos.x + (m_posInfos.y + 1)* WIDTH] = m_charInfos[0];
-			buffer[m_posInfos.x + m_posInfos.y * WIDTH] = m_charInfos[1];
-		}
-	
+	// Check borders
+	if (m_posInfos.x < 0)
+		m_realPosition.x = WIDTH;
+	else if (m_posInfos.x > WIDTH)
+		m_realPosition.x = 0;
+
+	if (m_posInfos.y < 0)
+		m_realPosition.y = HEIGHT;
+	else if (m_posInfos.y > HEIGHT)
+		m_realPosition.y = 0;
+		
+	buffer[m_posInfos.x + m_posInfos.y * WIDTH] = m_charInfos[1];
+
+	if (m_posInfos.y +1 < HEIGHT && m_posInfos.y +1 > 0)
+		buffer[m_posInfos.x + (m_posInfos.y + 1)* WIDTH] = m_charInfos[0];
 }
